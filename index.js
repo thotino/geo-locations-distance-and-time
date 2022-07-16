b83a51065e8ce3f66dad089b22454d7828bae032
Thotino GOBIN-GANSOU <thotino@lalalab.com>
2022-07-16 09:05:40 +0200

    feat: introducing a middleware to verify the payload

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
+const { verifyRequest } = require('./middlewares')
 const { getDistanceAndTime } = require('./controllers/geo.controller')
 
 const app = express();
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.post('/api/distance_and_time', getDistanceAndTime)
+app.post('/api/distance_and_time', [ verifyRequest ], getDistanceAndTime)
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');

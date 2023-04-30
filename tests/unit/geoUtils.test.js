af5b950451e37950b4d725c3c47d60faf2775794
Thotino GOBIN-GANSOU <gthomario@yahoo.com>
2023-04-30 19:52:55 +0200

    test: correct test suites

--- tests/unit/geoUtils.test.js
+++ tests/unit/geoUtils.test.js
@@ -1,53 +1,36 @@
-const { getCountry, getTimezone, getDistanceWithGMaps, getDistanceWithGeoLib } = require('../../utils/geoUtils')
-const ClientMock = require('@googlemaps/google-maps-services-js').Client
+jest.mock("@googlemaps/google-maps-services-js");
+const {
+  getCountry,
+  getTimezone,
+  getDistanceWithGMaps,
+  getDistanceWithGeoLib,
+} = require("../../utils/geoUtils");
 
-// const setupGoogleMock = () => {
-//   /** * Mock Google Maps JavaScript API ***/
-//   const google = {
-//     maps: {
-//       places: {
-//         AutocompleteService: () => {},
-//         PlacesServiceStatus: {
-//           INVALID_REQUEST: 'INVALID_REQUEST',
-//           NOT_FOUND: 'NOT_FOUND',
-//           OK: 'OK',
-//           OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
-//           REQUEST_DENIED: 'REQUEST_DENIED',
-//           UNKNOWN_ERROR: 'UNKNOWN_ERROR',
-//           ZERO_RESULTS: 'ZERO_RESULTS'
-//         }
-//       },
-//       reverseGeocode: () => {},
-//       Geocoder: () => {},
-//       GeocoderStatus: {
-//         ERROR: 'ERROR',
-//         INVALID_REQUEST: 'INVALID_REQUEST',
-//         OK: 'OK',
-//         OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
-//         REQUEST_DENIED: 'REQUEST_DENIED',
-//         UNKNOWN_ERROR: 'UNKNOWN_ERROR',
-//         ZERO_RESULTS: 'ZERO_RESULTS'
-//       }
-//     }
-//   }
-//   // global.window.google = google
-// }
+const ClientMock = require("@googlemaps/google-maps-services-js").Client;
 
-describe('[unit] helpers `GeoUtils`', () => {
-  describe('[unit] function `getCountry`', () => {
+const reverseGeocodeMock = jest.spyOn(ClientMock.prototype, "reverseGeocode");
+const timezoneMock = jest.spyOn(ClientMock.prototype, "timezone");
 
-    test('should fail if no coordinate is provided', async () => {
-      await expect(getCountry({ latitude: null, longitude: null })).rejects.toThrow('ERR_NO_VALID_PARAMETER_PROVIDED')
-      await expect(getCountry({ latitude: 0, longitude: null })).rejects.toThrow('ERR_NO_VALID_PARAMETER_PROVIDED')
-    })
+describe("[unit] helpers `GeoUtils`", () => {
+  describe("[unit] function `getCountry`", () => {
+    test("should fail if no coordinate is provided", async () => {
+      await expect(
+        getCountry({ latitude: null, longitude: null })
+      ).rejects.toThrow("ERR_NO_VALID_PARAMETER_PROVIDED");
+      await expect(
+        getCountry({ latitude: 0, longitude: null })
+      ).rejects.toThrow("ERR_NO_VALID_PARAMETER_PROVIDED");
+    });
 
-    test.only('should fail if GMaps API fails', async () => {
-      console.log({ ClientMock })
-      ClientMock.mockImplementation(() => ({
-        reverseGeocode: () => { throw new Error('ERR_RANDOM_ERROR_FROM_MODULE') }
-      }))
-      await expect(getCountry({ latitude: 0.01, longitude: 0.01 })).rejects
-      await (ClientMock.reverseGeocode).toHaveBeenCalledTimes(1)
-    })
-  })
-})
+    test("should fail if GMaps API fails", async () => {
+      reverseGeocodeMock.mockRejectedValue(
+        new Error("ERR_RANDOM_ERROR_FROM_MODULE")
+      );
+
+      await expect(
+        getCountry({ latitude: 0.01, longitude: 0.01 })
+      ).rejects.toThrowError("ERR_RANDOM_ERROR_FROM_MODULE");
+      expect(reverseGeocodeMock).toHaveBeenCalledTimes(1);
+    });
+  });
+});
